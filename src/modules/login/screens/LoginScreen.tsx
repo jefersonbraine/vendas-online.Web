a594a05e3826c33c8688894967c3846db7bfce56
Jeferson <jefeleal363@gmail.com>
2024-08-15 14:40:22 -0300

    feat: login page

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,15 @@
+import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage } from "../styles/loginScreen.styles";
+
+const LoginScreen = () => {
+    return (<ContainerLoginScreen>
+        <BackgroundImage src="./background.png" />
+        <ContainerLogin>
+            <LimitedContainer>
+                 <LogoImage src="./logo.png" />
+            </LimitedContainer>
+        </ContainerLogin>
+
+    </ContainerLoginScreen>);
+};
+
+export default LoginScreen; 
