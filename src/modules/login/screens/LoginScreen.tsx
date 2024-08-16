9ee288cc56f12bcd7eba0e320182326f5741d406
Jeferson <jefeleal363@gmail.com>
2024-08-16 13:05:39 -0300

    feat: state login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,17 +1,47 @@
+import { useState } from "react";
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-    return (<ContainerLoginScreen>
-        
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    };
+
+    const handleLogin = () => {
+        alert(` username: ${username}, password: ${password}`);
+    }
+
+    return (
+        <ContainerLoginScreen>
         <ContainerLogin>
             <LimitedContainer>
                 <LogoImage src="./logo.png" />
                 <TitleLogin level={2} type="secondary">LOGIN</TitleLogin>
-                <Input title ="Usuário" />
-                <Input title="Senha" />
-                <Button type="primary" margin="64px 0px 16px 0px">ENTRAR</Button>
+                    <Input
+                        title="Usuário"
+                        margin="32px 0px 0px"
+                        onChange={handleUsername}
+                        value={username} />
+                    <Input
+                        type="password"
+                        title="Senha"
+                        margin="32px 0px 0px"
+                        onChange={handlePassword}
+                        value={password} />
+                    <Button
+                        type="primary"
+                        margin="64px 0px 16px 0px"
+                        onClick={handleLogin}>ENTRAR
+                    </Button>
             </LimitedContainer>
         </ContainerLogin>
         <BackgroundImage src="./background.png" />
