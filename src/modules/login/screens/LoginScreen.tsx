de4056943a1c792712e23d864b79cb96ce11e5b0
Jeferson <jefeleal363@gmail.com>
2024-08-16 13:31:53 -0300

    feat: insert axios

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,23 +1,41 @@
 import { useState } from "react";
+
+import axios from 'axios';
+
 import Button from "../../../shared/buttons/button/Button";
 import Input from "../../../shared/inputs/input/input";
 import { BackgroundImage, ContainerLogin, ContainerLoginScreen, LimitedContainer, LogoImage, TitleLogin } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
 
-    const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(event.target.value);
+    const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
     };
 
     const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(event.target.value);
     };
 
-    const handleLogin = () => {
-        alert(` username: ${username}, password: ${password}`);
+    const handleLogin = async () => {
+        await axios({
+            method: "post",
+            url: "http://localhost:8080/auth",
+            data: {
+                    "email": email,
+                    "password": password,
+                },
+        })
+            .then((result) => {
+                alert(`Fez login ${result.data.accessToken}`);
+                return result.data;
+            })
+            .catch(() => {
+            alert('Login ou senha inválidos');
+        });
+        
     }
 
     return (
@@ -29,8 +47,8 @@ const LoginScreen = () => {
                     <Input
                         title="Usuário"
                         margin="32px 0px 0px"
-                        onChange={handleUsername}
-                        value={username} />
+                        onChange={handleEmail}
+                        value={email} />
                     <Input
                         type="password"
                         title="Senha"
